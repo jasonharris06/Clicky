2fda6c11e241d7587a56e7316327f6172e83ebdc
JazHar <jasonharris06@gmail.com>
2019-04-08 18:35:01 -0600

    Updates

---
+++ precache-manifest.737fb68c076443f14ef03714a582e540.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a113ecde1b8ed696dd4",
+    "url": "/jasonharris06/clicky.git/static/js/runtime~main.731d0980.js"
+  },
+  {
+    "revision": "c3bc380f2e0077d14da8",
+    "url": "/jasonharris06/clicky.git/static/js/main.1ecc7686.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/jasonharris06/clicky.git/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "c3bc380f2e0077d14da8",
+    "url": "/jasonharris06/clicky.git/static/css/main.dbd68575.chunk.css"
+  },
+  {
+    "revision": "8215f98c2933888a44c4897beb1f10bb",
+    "url": "/jasonharris06/clicky.git/index.html"
+  }
+];
